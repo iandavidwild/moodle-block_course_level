560aec1cab445cc99129d0732ca652beffc2bb99
iandavidwild <i.wild@ulcc.ac.uk>
2012-10-09 10:22:13 +0100

    Initial commit. Displays sample data in a tree view.

---
+++ block_course_level.php
@@ -0,0 +1,95 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Main block file
+ *
+ * @package    block
+ * @subpackage course_level
+ * @copyright  2012 University of London Computer Centre
+ * @author     Ian Wild {@link http://moodle.org/user/view.php?id=325899}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * This class builds a course level block on any page page which loads courses and units into a tree.
+ *
+ * @copyright 2012 Ian Wild
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_course_level extends block_base {
+
+    /**
+     * Standard init function, sets block title and version number
+     *
+     * @return void
+     */
+    public function init() {
+        $this->title = get_string('courselevel', 'block_course_level');
+    }
+
+    /**
+     * Standard specialization function
+     *
+     * @return void
+     */
+    public function specialization() {
+        $this->title = get_string('courselevel', 'block_course_level');
+    }
+
+    /**
+     * Standard get content function returns $this->content containing the block HTML etc
+     *
+     * @return stdClass
+     */
+    public function get_content() {
+
+        global $CFG, $USER, $PAGE, $OUTPUT;
+
+        if ($this->content !== NULL) {
+            return $this->content;
+        }
+        if (empty($this->instance)) {
+            return null;
+        }
+
+        $this->content->text = '';
+        $this->content->footer = '';
+        if (isloggedin() && !isguestuser()) {   // Show the block
+            $this->content = new stdClass();
+
+            //TODO: add capability check here?
+
+            $renderer = $this->page->get_renderer('block_course_level');
+            $this->content->text = $renderer->course_level_tree();
+            $this->content->footer = '';
+
+        }
+        return $this->content;
+    }
+
+    /**
+     * Standard function - does the block allow configuration for specific instances of itself
+     * rather than sitewide?
+     *
+     * @return bool false
+     */
+    public function instance_allow_config() {
+        return false;
+    }
+}
---
+++ renderer.php
@@ -0,0 +1,89 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Print course level tree
+ *
+ * @package    block_course_level
+ * @copyright  2012 University of London Computer Centre
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+
+require_once($CFG->dirroot . '/blocks/course_level/lib.php');
+
+class block_course_level_renderer extends plugin_renderer_base {
+
+    /**
+     * Prints course level tree view
+     * @return string
+     */
+    public function course_level_tree() {
+        return $this->render(new course_level_tree);
+    }
+
+    public function render_course_level_tree(course_level_tree $tree) {
+        $module = array('name'=>'block_course_level', 'fullpath'=>'/blocks/course_level/module.js', 'requires'=>array('yui2-treeview'));
+        if (empty($tree) ) {
+            $html = $this->output->box(get_string('nocourses', 'block_course_level'));
+        } else {
+            $htmlid = 'course_level_tree_'.uniqid();
+            $this->page->requires->js_init_call('M.block_course_level.init_tree', array(false, $htmlid));
+            $html = '<div id="'.$htmlid.'">';
+            $html .= $this->htmllize_tree($tree->courses);
+            $html .= '</div>';
+        }
+
+        return $html;
+    }
+
+    /**
+     * Converts the course tree into something more meaningful.
+     *
+     * @param $tree
+     * @param int $indent
+     * @return string
+     */
+    protected function htmllize_tree($tree, $indent=0) {
+        $yuiconfig = array();
+        $yuiconfig['type'] = 'html';
+
+        $result = '<ul>';
+
+        foreach ($tree as $node) {
+            $children = $node->get_children();
+
+            // TODO this needs to use html_writer to output
+            if($children != null) {
+                $result .= '<li yuiConfig=\''.json_encode($yuiconfig).'\'><div>'.s($node->get_shortname()).'</div> '.$this->htmllize_tree($children, $indent+1).'</li>';
+            } else {
+                $result .= '<li yuiConfig=\''.json_encode($yuiconfig).'\'><div>'.s($node->get_shortname()).'</div></li>';
+            }
+
+        }
+        $result .= '</ul>';
+
+        return $result;
+    }
+}
+
+
