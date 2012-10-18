cde14a7922cdd3ff61556a0783e5ea573c42d3b8
Ian Wild <i.wild@ulcc.ac.uk>
2012-10-18 14:19:43 +0100

    Finish implementing 'programme courses' HTML dialog.

--- view.php
+++ view.php
@@ -178,6 +178,8 @@ if($tab == PROGRAMMES_VIEW) {
 
         $programmesprinted = array();
 
+        $dlgIds = array();
+
         foreach ($programmelist as $course) {
             if (in_array($course->id, $programmesprinted)) {
                 continue;
@@ -191,30 +193,37 @@ if($tab == PROGRAMMES_VIEW) {
 
             $data[] = $link;
 
-            // TODO IDW 18/10/12 This is not fully implemented!
             // Output the links to this programme's courses:
-            $progcourses = $ual_mis->get_programme_courses($course->id);
+            $progcourses = $ual_mis->get_programme_courses($course->shortname);
 
-            if(!empty($progcourses)) {
-                $dlgTitle = html_writer::tag('div', get_string('courses', 'block_course_level'), array('class' => 'dlgTitle', 'id' => 'courses-1'));
+            if(!empty($progcourses)  && $progcourses->valid() ) {
+                $dlgId = $course->id;
+                $dlgTitle = html_writer::tag('div', get_string('programme_courses', 'block_course_level'), array('class' => 'dlgTitle', 'id' => 'course-'.$dlgId));
                 $links = array(); // Start with an empty list of links
                 foreach($progcourses as $progcourse) {
-                    $links[] = html_writer::link('#', $progcourse->fullname);
+                    $links[] = html_writer::link(new moodle_url('/course/view.php?id='.$progcourse->id), $progcourse->fullname);
                 }
                 // Implode the list of links and separate with a <br/>...
                 $contentBox = implode('<br/>', $links);
                 $contentBox = html_writer::tag('div', $contentBox, array('class' => 'contentBox'));
 
-                $cellContents = html_writer::tag('div', $dlgTitle.$contentBox, array('class' => 'yui3-overlay-loading', 'id' => 'unitoverlay-' . $course->id));
+                $cellContents = html_writer::tag('div', $dlgTitle.$contentBox, array('class' => 'yui3-overlay-loading', 'id' => 'courseoverlay-' . $dlgId));
 
                 $data[] = $cellContents;
+
+                // Remember what all the course dialog ids are. There will be one per course...
+                $dlgIds[] = $dlgId;
             } else {
-                // TODO Nothing to display
                 $data[] = get_string('nothingtodisplay');
             }
 
             $table->add_data($data);
         }
+
+        // Load up relevant Javascript, passing the course id's of all of the course units displayed on the page...
+        $PAGE->requires->yui_module('moodle-block_course_level-courses',
+            'M.blocks_course_level.init_courses',
+            array(array('courseids' => $dlgIds)));
 
         $table->print_html();
     }
@@ -376,11 +385,6 @@ if($tab == PROGRAMMES_VIEW) {
 print_tabbed_table_end();
 
 echo $OUTPUT->footer();
-
-function print_courses_table() {
-
-
-}
 
 /**
  * Ensure HTML is correctly formed.
