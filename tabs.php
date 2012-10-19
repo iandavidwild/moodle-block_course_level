4d7824dfe67a8989926a328a27e9c448cf87193c
Ian Wild <i.wild@ulcc.ac.uk>
2012-10-19 15:50:12 +0100

    Added a heading.

--- tabs.php
+++ tabs.php
@@ -1,4 +1,34 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    block
+ * @subpackage course_level
+ * @copyright  2012 University of London Computer Centre
+ * @author     Ian Wild {@link http://moodle.org/user/view.php?id=325899}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Tabs on 'All Courses & Programmes' page
+ *
+ * @copyright 2012 Ian Wild
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
     if (!isset($sortorder)) {
         $sortorder = '';
     }
